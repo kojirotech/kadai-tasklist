483d14b1c06190d2f9f8a32253ddbaecb55ad355
kojirotech <techacademy3618@gmail.com>
2018-06-03 22:34:24 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     @tasks = Task.all.page(params[:page])
   end
@@ -16,10 +18,10 @@ class TasksController < ApplicationController
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -37,10 +39,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+      @task.destroy
+      flash[:success] = 'Task は正常に削除されました'
+      redirect_back(fallback_location: root_path)
   end
   
   private
