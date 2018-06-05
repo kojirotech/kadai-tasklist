feda4a4fa627f61877c59622284995a4940a6c7b
kojirotech <techacademy3618@gmail.com>
2018-06-05 13:49:27 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to root_url
     else
-      @task = current_user.tasks.order('created_at DESC').page(params[:page])
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render 'toppages/index'
     end
